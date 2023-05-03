ea48cc9b96fb4723e9be8f5f2b2390c42768f1fb
mishan4ik <60795949+mishan4ik@users.noreply.github.com>
2023-05-03 10:24:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let Temp = document.querySelector(".block_temperature");
+let Freezing = document.querySelector(".block_freezing");
+let cooling = document.querySelector(".block_cooling");
+let defrosting = document.querySelector(".block_defrosting");
+let freshness = document.querySelector(".block_freshness")
+
+Temp.onclick = function(){
+
+    window.location = "./temperature.html"
+
+}
+Freezing.onclick = function(){
+
+    window.location = "./freezing.html"
+
+}
+cooling.onclick = function(){
+
+    window.location = "./Super_cool.html"
+
+}
+
+defrosting.onclick = function(){
+
+    window.location = "./defrosting.html"
+
+}
+
+freshness.onclick = function(){
+
+    window.location = "./fresh.html"
+
+}
+
